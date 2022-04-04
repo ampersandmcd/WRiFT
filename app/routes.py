a570d32d31b492ec00896c4a20fdfa5ac6a0eb52
Andrew McDonald <andrewmcdonald27@gmail.com>
2022-04-03 23:07:47 -0400

    adds wildfire fact and prevention tips

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from flask import current_app as app
 import pandas as pd
 import json
+import random
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
@@ -11,6 +12,27 @@ from app.modeling.economic_impacts import EconomicImpactCalculator
 
 token = open("app/static/.mapbox_token").read()
 px.set_mapbox_access_token(token)
+
+# from https://www.iii.org/article/facts-about-wildfires
+fun_facts = [
+    "Wildfires occur in 38 states; California is the state most associated with wildfires and, in fact, eight of the 10 most costly wildfires in the U.S. have occurred there. That said, Texas has been known to have twice the wildfires as California in a given year and 38 of U.S. states have areas at risk.",
+    "Wildfires like dry conditions; drought conditions, dry undergrowth and the presence of combustible and flammable materials contribute to wildfire hazard.",
+    "Wildfires are more dangerous in combination with development; the risk of damage increases as housing and business development expands into the wildfire-prone wildland-urban interface (WUI), such as mountain, foothill or grassland areas.",
+    "Wildfires spread mostly due to wind; direct flame contact and radiant heat from a wildfire can ignite combustible materials. However, research has shown that homes burned during wildfires most frequently catch fire from live embers (or 'firebrands') that are blown by the wind.",
+    "Wildfires thrive on house 'togetherness'; because of the dangers of the embers, close proximity of homes and presence of combustible features both increase the chances of a home going up in flames. Fire spreads rapidly when homes are less than 15 feet apart, making homes that are clustered near others more likely to burn. Features like fences and attached decks made from combustible materials often hasten the spread of fire."
+]
+prevention_facts = [
+    "maintaining five feet of non-combustible 'defensible space' around your home; keep a five-foot diameter space of gravel, brick, or concrete in the area adjacent to your home.",
+    "maintaining an expanded 'defensible space' between five and 30 feet from your home; keeping this area as unattractive to wildfires as possible will reduce the risk. Move trailers/RVs and storage sheds from area, or build defensible space (see above) around these items. Remove shrubs under trees, prune branches that overhang your roof, thin trees, and remove dead vegetation.",
+    "using non-combustible siding and maintaining a six-inch ground-to-siding clearance.",
+    "regularly cleaning from your roof and gutters to keep debris from being ignited by wind-blown embers. Use noncombustible gutter covers.",
+    "getting a Class A fire-rated roof; Class A roofing products offer the best protection for homes.",
+    "using non-combustible fences and gates; burning fencing can generate embers and cause direct flame contact to your home.",
+    "covering vents and creating soffited eaves; use 1/8-inch mesh to cover vents, and box-in (create soffits) on open eaves to keep embers out.",
+    "using multi-pane, tempered glass windows; close windows when a wildfire threatens.",
+    "fireproofing the deck; at a minimum, use deck boards that comply with California requirements for new construction in wildfire-prone areas. Remove combustibles from under deck, and maintain effective defensible space around the deck.",
+    "keeping combustibles far away from the house; combustible structures in the yard such as wood, plastic or plastic-wood playground equipment should be at least 30 feet away from the house. Experts indicate that evergreen trees, palms and eucalyptus trees have more combustible qualities than others—keep this type of vegetation 100 feet away from the house."
+]
 
 impactCalculator = EconomicImpactCalculator()
 
@@ -103,7 +125,9 @@ def index():
         fig = go.Figure(data=data, layout=layout)
         graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-        return render_template("index.html", graph_json=graph_json, impacts=False, num_damaged=0, num_destroyed=0)
+        return render_template("index.html", graph_json=graph_json, impacts=False,
+                               num_damaged=0, num_destroyed=0,
+                               fun_fact=random.choice(fun_facts), prevention_fact=random.choice(prevention_facts))
 
     if request.method == "POST":
         #
@@ -155,7 +179,9 @@ def index():
         fig = go.Figure(data=data, layout=layout)
         graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-        return render_template("index.html", graph_json=graph_json, impacts=True, num_damaged=num_damaged, num_destroyed=num_destroyed)
+        return render_template("index.html", graph_json=graph_json, impacts=True,
+                               num_damaged=num_damaged, num_destroyed=num_destroyed,
+                               fun_fact=random.choice(fun_facts), prevention_fact=random.choice(prevention_facts))
 
 
 @app.route("/about")
